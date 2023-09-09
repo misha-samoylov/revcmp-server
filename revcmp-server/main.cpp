96e14d6664ce8aec5e69c2bd731f639d2a3552e5
Misha Samoylov <mikhailsamoylov95@gmail.com>
2023-09-09 18:40:06 +0700

    Refactored

--- revcmp-server/main.cpp
+++ revcmp-server/main.cpp
@@ -1,40 +1,11 @@
-/*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-
- // ----------------------------------------------------------------------
- // RakNet version 1.0
- // Filename ChatExample.cpp
- // Very basic chat engine example
- // ----------------------------------------------------------------------
-
+#include "RakPeerInterface.h"
+#include "BitStream.h"
 #include "MessageIdentifiers.h"
 
-#include "RakPeerInterface.h"
-#include "RakNetStatistics.h"
-#include "RakNetTypes.h"
-#include "BitStream.h"
-#include "RakSleep.h"
-#include "PacketLogger.h"
-#include <assert.h>
-#include <cstdio>
-#include <cstring>
-#include <stdlib.h>
-#include "Kbhit.h"
-#include <stdio.h>
-#include <string.h>
-#include "Gets.h"
-
-#include "BitStream.h"
-#include "RakNetTypes.h"
-
 #define SERVER_PORT 5040
+#define SERVER_COUNT_ALLOWED_CLIENTS 50
+#define SERVER_PASSWORD "Rumpelstiltskin"
+#define COUNT_SOCKET_DESCRIPTOR 1
 
 enum GameMessages {
 	ID_PLAYER_COORDINATES = ID_USER_PACKET_ENUM + 1
@@ -47,14 +18,63 @@ struct PlayerInfo {
 
 struct PlayerInfo gPlayerInfo[2];
 
-// We copy this from Multiplayer.cpp to keep things all in one file for this example
 unsigned char GetPacketIdentifier(RakNet::Packet* p);
-void UpdateNetwork(RakNet::RakPeerInterface *server)
+void UpdateNetwork(RakNet::RakPeerInterface* server);
+void SendPlayerPositions(RakNet::RakPeerInterface* server);
+
+int main()
 {
-	// GetPacketIdentifier returns this
+	ZeroMemory(gPlayerInfo, sizeof(gPlayerInfo));
+
+	RakNet::RakPeerInterface* server = RakNet::RakPeerInterface::GetInstance();
+	server->SetIncomingPassword(SERVER_PASSWORD, (int)strlen(SERVER_PASSWORD));
+	server->SetTimeoutTime(30000, RakNet::UNASSIGNED_SYSTEM_ADDRESS);
+
+	RakNet::SocketDescriptor socketDescriptors[COUNT_SOCKET_DESCRIPTOR];
+	socketDescriptors[0].port = SERVER_PORT;
+	socketDescriptors[0].socketFamily = AF_INET;
+
+	RakNet::StartupResult result = server->Startup(SERVER_COUNT_ALLOWED_CLIENTS, socketDescriptors, COUNT_SOCKET_DESCRIPTOR);
+	server->SetMaximumIncomingConnections(SERVER_COUNT_ALLOWED_CLIENTS);
+
+	if (result != RakNet::RAKNET_STARTED) {
+		puts("Server failed to start. Terminating\n");
+		exit(1);
+	}
+
+	printf("reVCMP server started on port: %d\n", SERVER_PORT);
+
+	while (true) {
+		Sleep(5);
+		UpdateNetwork(server);
+		SendPlayerPositions(server);
+	}
+
+	server->Shutdown(300);
+	RakNet::RakPeerInterface::DestroyInstance(server);
+
+	return 0;
+}
+
+unsigned char GetPacketIdentifier(RakNet::Packet* p)
+{
+	if (p == 0) {
+		return 255;
+	}
+
+	if ((unsigned char)p->data[0] == ID_TIMESTAMP) {
+		RakAssert(p->length > sizeof(RakNet::MessageID) + sizeof(RakNet::Time));
+		return (unsigned char)p->data[sizeof(RakNet::MessageID) + sizeof(RakNet::Time)];
+	}
+
+	return (unsigned char)p->data[0];
+}
+
+
+void UpdateNetwork(RakNet::RakPeerInterface* server)
+{
 	unsigned char packetIdentifier;
 
-	RakNet::SystemAddress clientID;
 	RakNet::Packet* p;
 
 	for (p = server->Receive(); p; server->DeallocatePacket(p), p = server->Receive())
@@ -70,11 +90,9 @@ void UpdateNetwork(RakNet::RakPeerInterface *server)
 			printf("ID_DISCONNECTION_NOTIFICATION from %s\n", p->systemAddress.ToString(true));;
 			break;
 
-
 		case ID_NEW_INCOMING_CONNECTION:
 			// Somebody connected.  We have their IP now
 			printf("ID_NEW_INCOMING_CONNECTION from %s with GUID %s\n", p->systemAddress.ToString(true), p->guid.ToString());
-			clientID = p->systemAddress; // Record the player ID of the client
 
 			printf("Remote internal IDs:\n");
 			for (int index = 0; index < MAXIMUM_NUMBER_OF_INTERNAL_IDS; index++)
@@ -113,143 +131,32 @@ void UpdateNetwork(RakNet::RakPeerInterface *server)
 			myBitStream.Read(plrId);
 			myBitStream.ReadVector(x, y, z);
 
-			
-
 			gPlayerInfo[plrId].id = plrId;
 			gPlayerInfo[plrId].x = x;
 			gPlayerInfo[plrId].y = y;
 			gPlayerInfo[plrId].z = z;
 
-			printf("received plrId: %d %f %f %f \n", plrId, x, y, z);
+			// printf("received plrId: %d %f %f %f\n", plrId, x, y, z);
 
 			break;
 		}
-		//default:
-			// The server knows the static data of all clients, so we can prefix the message
-			// With the name data
-		//	printf("%s\n", p->data);
-
-			// Relay the message.  We prefix the name for other clients.  This demonstrates
-			// That messages can be changed on the server before being broadcast
-			// Sending is the same as before
-		//	sprintf(message, "%s", p->data);
-		//	server->Send(message, (const int) strlen(message)+1, HIGH_PRIORITY, RELIABLE_ORDERED, 0, p->systemAddress, true);
-
-		break;
+		default:
+			break;
 		}
-
 	}
-};
+}
 
-void SendPlayerPositions(RakNet::RakPeerInterface *server)
+void SendPlayerPositions(RakNet::RakPeerInterface* server)
 {
 	for (int i = 0; i < 2; i++) {
 		RakNet::BitStream bsOut;
 		bsOut.Write((RakNet::MessageID)ID_PLAYER_COORDINATES);
 		bsOut.Write((int32_t)i);
 		bsOut.WriteVector(
-			(float)gPlayerInfo[i].x, 
+			(float)gPlayerInfo[i].x,
 			(float)gPlayerInfo[i].y,
 			(float)gPlayerInfo[i].z
 		);
 		server->Send(&bsOut, LOW_PRIORITY, UNRELIABLE, 0, RakNet::UNASSIGNED_SYSTEM_ADDRESS, true);
 	}
-}
-
-int main(void)
-{
-	// Pointers to the interfaces of our server and client.
-	// Note we can easily have both in the same program
-	RakNet::RakPeerInterface* server = RakNet::RakPeerInterface::GetInstance();
-	RakNet::RakNetStatistics* rss;
-	server->SetIncomingPassword("Rumpelstiltskin", (int)strlen("Rumpelstiltskin"));
-	server->SetTimeoutTime(30000, RakNet::UNASSIGNED_SYSTEM_ADDRESS);
-
-	ZeroMemory(gPlayerInfo, sizeof(gPlayerInfo));
-
-	// Record the first client that connects to us so we can pass it to the ping function
-	RakNet::SystemAddress clientID = RakNet::UNASSIGNED_SYSTEM_ADDRESS;
-
-	puts("Starting server.");
-
-	// Starting the server is very simple.  2 players allowed.
-	// 0 means we don't care about a connectionValidationInteger, and false
-	// for low priority threads
-	// I am creating two socketDesciptors, to create two sockets. One using IPV6 and the other IPV4
-	RakNet::SocketDescriptor socketDescriptors[2];
-	socketDescriptors[0].port = SERVER_PORT;
-	socketDescriptors[0].socketFamily = AF_INET; // Test out IPV4
-	socketDescriptors[1].port = SERVER_PORT;
-	socketDescriptors[1].socketFamily = AF_INET6; // Test out IPV6
-
-	bool b = server->Startup(4, socketDescriptors, 2) == RakNet::RAKNET_STARTED;
-	server->SetMaximumIncomingConnections(4);
-	if (!b)
-	{
-		printf("Failed to start dual IPV4 and IPV6 ports. Trying IPV4 only.\n");
-
-		// Try again, but leave out IPV6
-		b = server->Startup(4, socketDescriptors, 1) == RakNet::RAKNET_STARTED;
-		if (!b)
-		{
-			puts("Server failed to start.  Terminating.");
-			exit(1);
-		}
-	}
-
-	server->SetOccasionalPing(true);
-	server->SetUnreliableTimeout(1000);
-
-	DataStructures::List< RakNet::RakNetSocket2* > sockets;
-	server->GetSockets(sockets);
-
-	printf("Socket addresses used by RakNet:\n");
-	for (unsigned int i = 0; i < sockets.Size(); i++)
-	{
-		printf("%i. %s\n", i + 1, sockets[i]->GetBoundAddress().ToString(true));
-	}
-
-	printf("\nMy IP addresses:\n");
-	for (unsigned int i = 0; i < server->GetNumberOfAddresses(); i++)
-	{
-		RakNet::SystemAddress sa = server->GetInternalID(RakNet::UNASSIGNED_SYSTEM_ADDRESS, i);
-		printf("%i. %s (LAN=%i)\n", i + 1, sa.ToString(false), sa.IsLANAddress());
-	}
-
-	printf("\nMy GUID is %s\n", server->GetGuidFromSystemAddress(RakNet::UNASSIGNED_SYSTEM_ADDRESS).ToString());
-	puts("'quit' to quit. 'stat' to show stats. 'ping' to ping.\n'pingip' to ping an ip address\n'ban' to ban an IP from connecting.\n'kick to kick the first connected player.\nType to talk.");
-	char message[2048];
-
-	// Loop for input
-	while (true)
-	{
-		Sleep(5);
-
-		UpdateNetwork(server);
-		SendPlayerPositions(server);
-		
-	}
-
-	server->Shutdown(300);
-	// We're done with the network
-	RakNet::RakPeerInterface::DestroyInstance(server);
-
-	return 0;
-}
-
-// Copied from Multiplayer.cpp
-// If the first byte is ID_TIMESTAMP, then we want the 5th byte
-// Otherwise we want the 1st byte
-unsigned char GetPacketIdentifier(RakNet::Packet* p)
-{
-	if (p == 0)
-		return 255;
-
-	if ((unsigned char)p->data[0] == ID_TIMESTAMP)
-	{
-		RakAssert(p->length > sizeof(RakNet::MessageID) + sizeof(RakNet::Time));
-		return (unsigned char)p->data[sizeof(RakNet::MessageID) + sizeof(RakNet::Time)];
-	}
-	else
-		return (unsigned char)p->data[0];
 }
